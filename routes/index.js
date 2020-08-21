d32cecf91cdf863833b2879d8ef507ae885a4688
Souvik Paul <Souvikkaka0@gmail.com>
2020-08-21 14:25:45 +0530

    Firebase Connectivity
    Frontend theke data niye khali pass koriye dis

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var adminController = require('../controllers/admincontroller');
 var router = express.Router();
+var firebase = require('firebase');
+var fire = firebase.initializeApp({
+    apiKey: "AIzaSyB0M8Qrwze4wrkRKz6NBZQ5N4ZeVMzsCCU",
+    authDomain: "groceryapp-dbf52.firebaseapp.com",
+    databaseURL: "https://groceryapp-dbf52.firebaseio.com",
+    projectId: "groceryapp-dbf52",
+    storageBucket: "groceryapp-dbf52.appspot.com",
+    messagingSenderId: "1019129350835",
+    appId: "1:1019129350835:web:3e906efe26a991f1747ec6",
+    measurementId: "G-0W3P8WERJT"
+  });
 
 router.get('/',function(req,res,next){
     res.render('home',{title: 'THIS IS HOME PAGE.'});
@@ -14,4 +25,20 @@ router.post('/adminlogincheck', adminController.AdminLoginCheck);
 router.get('/adminhome', function(req, res, next) {
     res.render('./adminpart/adminhome');
 });
+router.get('/productadd', function(req, res, next) {
+    res.render('./adminpart/productadd');
+});
+router.post('/insertproduct',function(req, res, next) {
+	var database = fire.database();   //get data from frontend and put here
+	fire.database().ref('product'+"vbnnmm,,").set({
+		productname:"myname",
+		productprice:"Your name",
+		productdes:"My description",
+		producturl:"bnjnj",
+		productcat:"Vegetable  122"
+
+
+	});
+	res.send("hellll");
+})
 module.exports = router;
